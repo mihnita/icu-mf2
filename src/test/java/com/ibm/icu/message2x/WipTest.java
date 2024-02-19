01b2d4fc3cf880f6501401cb708230a59c7b65dd
Mihai Nita <nmihai_2000@yahoo.com>
2024-02-19 13:21:56 -0800

    Tinker a bit with the test run conditions

--- src/test/java/com/ibm/icu/message2x/WipTest.java
+++ src/test/java/com/ibm/icu/message2x/WipTest.java
@@ -16,7 +16,10 @@ import org.junit.runners.JUnit4;
 import com.ibm.icu.message2x.Token.Type;
 
 @RunWith(JUnit4.class)
+@SuppressWarnings({ "static-method", "javadoc" })
 public class WipTest {
+  
+  static final boolean IGNORE_OK = true;
 	
   @Rule
   public ErrorCollector collector = new ErrorCollector();
@@ -40,6 +43,11 @@ public class WipTest {
       return new MiniToken<>(other.getKind(), other.getValue());
     }
     
+    @Override
+    public int hashCode() {
+      return Objects.hash(kind, value);
+    }
+
     @Override
     public boolean equals(Object obj) {
       if (this == obj)
@@ -73,7 +81,9 @@ public class WipTest {
     final List<MiniToken<?>> expectedTokens;
 
     static enum Test {
-      SKIP, RUN, TODO;
+      SKIP, // These are failing, but skip them for now
+      WIP, // Work in progress. Failing, but working on them right now 
+      OK; // Known to be working, but skip to avoid triggering breakpoints
     }
 
     TokenizerTestCase(String input, Test skip, List<MiniToken<?>> expectedTokens) {
@@ -82,7 +92,7 @@ public class WipTest {
       this.expectedTokens = expectedTokens;
     }
     TokenizerTestCase(String input, List<MiniToken<?>> expectedTokens) {
-      this(input, Test.RUN, expectedTokens);
+      this(input, Test.OK, expectedTokens);
     }
   }
 
@@ -124,7 +134,7 @@ public class WipTest {
               new MiniToken<>(Token.Type.STRING, "Hello\tworld"),
               new MiniToken<>(Token.Type.EOF, null)
               )),
-      new TokenizerTestCase("|Hello|world|", TokenizerTestCase.Test.TODO,
+      new TokenizerTestCase("|Hello|world|", TokenizerTestCase.Test.SKIP,
           Arrays.asList(
               new MiniToken<>(Token.Type.STRING, "Hello"),
               new MiniToken<>(Token.Type.EOF, null)
@@ -134,7 +144,7 @@ public class WipTest {
               new MiniToken<>(Token.Type.STRING, "Hello|world"),
               new MiniToken<>(Token.Type.EOF, null)
           )),
-      new TokenizerTestCase("|Hello\\\\|world|", TokenizerTestCase.Test.TODO,
+      new TokenizerTestCase("|Hello\\\\|world|", TokenizerTestCase.Test.SKIP,
           // Java makes this into Backslash-Backslash-Pipe, and that is what MF2 sees.
           // MF2 unescapes Backslash-Backslash to Backslash and puts it in the "raw string".
           // And coming after that the `|` marks the end of the string, it is not unescaped.
@@ -196,22 +206,30 @@ public class WipTest {
 
   @Test
   public void testTok() {
+    List<Token<?>> actual;
+    List<?> actualMini;
     for (TokenizerTestCase test : TOK_TEST) {
-      System.out.println("======================");
-      System.out.println(Utilities.str(test.input));
-      if (test.skip == TokenizerTestCase.Test.RUN) {
-        List<Token<?>> actual = Parser.tokenizeAll(test.input);
-        List<?> actualMini = actual.stream().map(MiniToken::fromToken).collect(Collectors.toList());
-//        assertEquals(test.expectedTokens, actualMini);
-        collector.checkThat(actualMini, IsEqual.equalTo(test.expectedTokens));
-      } else if (test.skip == TokenizerTestCase.Test.TODO) {
-        System.out.println("" + test.skip + " !!!");
-        List<Token<?>> actual = Parser.tokenizeAll(test.input);
-        List<?> actualMini = actual.stream().map(MiniToken::fromToken).collect(Collectors.toList());
-        System.out.println("Expected:");
-        System.out.println(actualMini.toString());
-      } else {
-        System.out.println("" + test.skip + " !!!");
+      switch (test.skip) {
+        case OK:
+          if (IGNORE_OK)
+            break;
+          actual = Parser.tokenizeAll(test.input);
+          actualMini = actual.stream().map(MiniToken::fromToken).collect(Collectors.toList());
+          collector.checkThat(actualMini, IsEqual.equalTo(test.expectedTokens));
+          break;
+        case WIP:
+          System.out.println("======================");
+          System.out.println("" + test.skip + " !!!");
+          System.out.println(Utilities.str(test.input));
+          actual = Parser.tokenizeAll(test.input);
+          actualMini = actual.stream().map(MiniToken::fromToken).collect(Collectors.toList());
+          System.out.println("Expected:");
+          System.out.println(actualMini.toString());
+          break;
+        case SKIP:
+          break;
+        default:
+          System.out.println("" + test.skip + " !!!");
       }
     }
   }
